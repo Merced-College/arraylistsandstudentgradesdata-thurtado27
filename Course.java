00820fff1095affb1b0b157dce8cc0b0e278df52
thurtado27 <155494987+thurtado27@users.noreply.github.com>
2026-02-20 18:23:58 +0000

    Completed Course Grades Analyzer lab

--- Course.java
+++ Course.java
@@ -1,7 +1,7 @@
 /*
  * Name: Trinity Hurtado, Rito Murllio
  * Date: 02/20/2026
- * Program: Course Grades Analyzer - 
+ * Program: Course Grades Analyzer - reads CSV grade totals and analyzes A percentages.
  */
 
 import java.util.ArrayList;
